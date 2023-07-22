a0d812a34467f7757c6def65bec3f6bdb796b62f
Gasterbuzzer <juliankoberg@hotmail.com>
2023-07-22 16:14:00 +0200

    Updated Loop

--- Frames/LeftFrame.py
+++ Frames/LeftFrame.py
@@ -52,12 +52,22 @@ class LeftFrame:
 
         # We have different Object Groups, we then go through each group and read the objects.
         for index, group_app in enumerate(self.app.objects):
-            print(f"{self.recurse_over_app(group_app)}\n")
+
+            return_value = self.recurse_over_app(group_app)
+
+            # Getting return value name:
+            object_name = next(iter(return_value))
+
+            if object_name == "sphere":
+                self.app.hierarchy.append(return_value)
+            else:
+                # We don't do anything for now.
+                print(f"H: {self.app.hierarchy}")
 
         # Now that we have loaded each object into memory, we can try rendering.
         print("\n")
 
-    def recurse_over_app(self, object_app, object_name="NoNameGiven", group_index="Recursive Function",
+    def recurse_over_app(self, object_app, object_name="NoNameGiven", group_index="RecursiveFunctionNoGroupGiven",
                          _index_possibly_given="", debug=False) -> dict:
         """
         Recurses over objects and gets, the according objects.
@@ -81,13 +91,13 @@ class LeftFrame:
             try:
                 position = object_app["position"]
             except KeyError:
-                print(f"Error Log: Sphere Position for {group_index} is not given.")
+                print(f"Weak Error Log: Sphere Position for {group_index} is not given.")
                 position = None
 
             try:
                 radius = object_app["radius"]
             except KeyError:
-                print(f"Error Log: Sphere Radius for {group_index} is not given.")
+                print(f"Weak Error Log: Sphere Radius for {group_index} is not given.")
                 radius = None
 
             try:
@@ -97,32 +107,32 @@ class LeftFrame:
                 try:
                     color_ambient = color_json["ambient"]
                 except KeyError:
-                    print(f"Error Log: Color Ambient for {object_app} {group_index} is not given.")
+                    print(f"Weak Error Log: Color Ambient for {object_app} {group_index} is not given.")
                     color_ambient = None
                 try:
                     color_diffuse = color_json["diffuse"]
                 except KeyError:
-                    print(f"Error Log: Color Diffuse for {object_app} {group_index} is not given.")
+                    print(f"Weak Error Log: Color Diffuse for {object_app} {group_index} is not given.")
                     color_diffuse = None
                 try:
                     color_specular = color_json["specular"]
                 except KeyError:
-                    print(f"Error Log: Color Specular for {object_app} {group_index} is not given.")
+                    print(f"Weak Error Log: Color Specular for {object_app} {group_index} is not given.")
                     color_specular = None
                 try:
                     color_reflected = color_json["reflected"]
                 except KeyError:
-                    print(f"Error Log: Color Reflected for {object_app} {group_index} is not given.")
+                    print(f"Weak Error Log: Color Reflected for {object_app} {group_index} is not given.")
                     color_reflected = None
                 try:
                     color_refracted = color_json["refracted"]
                 except KeyError:
-                    print(f"Error Log: Color Refracted for {object_app} {group_index} is not given.")
+                    print(f"Weak Error Log: Color Refracted for {object_app} {group_index} is not given.")
                     color_refracted = None
                 try:
                     color_shininess = color_json["shininess"]
                 except KeyError:
-                    print(f"Error Log: Color Shininess for {object_app} {group_index} is not given.")
+                    print(f"Weak Error Log: Color Shininess for {object_app} {group_index} is not given.")
                     color_shininess = None
 
                 # Create Object
@@ -131,7 +141,7 @@ class LeftFrame:
                 # Now we have the object, we continue
 
             except KeyError:
-                print(f"Error Log: Sphere Color for {group_index} is not given.")
+                print(f"Weak Error Log: Sphere Color for {group_index} is not given.")
                 color = None
 
             try:
@@ -140,7 +150,7 @@ class LeftFrame:
                 else:
                     i = index_possibly_given
             except KeyError:
-                print(f"Error Log: Sphere Index for {group_index} is not given.")
+                print(f"Weak Error Log: Sphere Index for {group_index} is not given.")
                 i = None
 
             return {"sphere": Sphere(position, radius, color, i)}
