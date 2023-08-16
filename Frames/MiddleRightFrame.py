305f6ef29fdd0c567b527d41edca0ce6b92cdd0d
Gasterbuzzer <juliankoberg@hotmail.com>
2023-08-16 20:11:47 +0200

    Allowing Image loading/generating

--- Frames/MiddleRightFrame.py
+++ Frames/MiddleRightFrame.py
@@ -1,20 +1,55 @@
 """Middle Right Frame"""
 import customtkinter
+from PIL import Image
 
 
 class MiddleRightFrame:
     """
     Middle Right Frame Class
     """
+
     def __init__(self, main_menu_reference) -> None:
         """
         Middle Right Frame Class (Showcase Image)
 
         :param main_menu_reference: Reference to Main Class (Not the Main Window)
         """
+        # References
+        self.main_menu_reference = main_menu_reference
+
+        self.height = 590
+        self.width = 790
+
+        self.difference_pad = 5
+
+        self.image_width = self.width - self.difference_pad * 3
+        self.image_height = self.height - self.difference_pad * 4
 
         # Main Frame
-        self.frame = customtkinter.CTkFrame(main_menu_reference.app)
-        self.frame.configure(border_width=2, height=590, width=790)
+        self.frame = customtkinter.CTkFrame(self.main_menu_reference.app)
+        self.frame.configure(border_width=2, height=self.height, width=self.width)
         self.frame.grid(row=1, column=1, padx=(0, 5), pady=5, sticky="nse")
         self.frame.grid_propagate(False)
+
+        # Image Display:
+
+        self.image = customtkinter.CTkLabel(self.frame, text="")
+        self.image.grid(row=0, column=0, padx=self.difference_pad, pady=self.difference_pad)
+
+    def update_image(self, image=None) -> None:
+        """
+        Updates the image to a new one.
+        :param image: Image to display (can be None)
+        """
+
+        if image is None:
+            background_image = Image.open("data/generated/generated.jpg").resize((self.image_width, self.image_height))
+
+            image_ctk_b = customtkinter.CTkImage(background_image, size=(self.image_width, self.image_height))
+
+            self.image.configure(image=image_ctk_b)
+            return
+
+        else:
+            # If not, we will decide our own.
+            self.image.configure(image=image)
--- Frames/TopRightFrame.py
+++ Frames/TopRightFrame.py
@@ -1,5 +1,8 @@
 """Top Right Frame"""
 import customtkinter
+from PIL import Image
+import subprocess
+import sys
 
 
 class TopRightFrame:
@@ -15,7 +18,10 @@ class TopRightFrame:
         """
 
         # Main Frame
-        self.frame = customtkinter.CTkFrame(main_menu_reference.app)
+
+        self.main_menu_reference = main_menu_reference
+
+        self.frame = customtkinter.CTkFrame(self.main_menu_reference.app)
         self.frame.configure(border_width=2, height=100, width=790)
         self.frame.grid(row=0, column=1, padx=(0, 5), pady=5, sticky="nw")
         self.frame.grid_propagate(False)
@@ -29,16 +35,20 @@ class TopRightFrame:
         self.format_label = customtkinter.CTkLabel(self.frame, text="File Format: ", justify="left",
                                                    width=20, font=("Franklin Gothic Medium", 22))
 
-        self.format_label.grid(row=0, column=0, padx=15, pady=(10, 0), columnspan=4)
+        self.format_label.grid(row=0, column=0, padx=15, pady=(10, 0))
 
         self.format_select_box = customtkinter.CTkComboBox(self.frame,
                                                            values=self.available_formats, width=200, height=24,
                                                            command=self.update_currently_selected_format,
                                                            state="readonly")
         self.format_select_box.set(self.currently_selected_format)
-        self.format_select_box.grid(row=1, column=0, padx=(5, 0), pady=(10, 0), columnspan=4)
+        self.format_select_box.grid(row=1, column=0, padx=(5, 0), pady=(10, 0))
 
         # Generate image without saving
+
+        self.save_button_without_close = customtkinter.CTkButton(master=self.frame, text="Generate Image",
+                                                                 command=self.generate_image, width=150)
+        self.save_button_without_close.grid(row=0, column=1, padx=25, pady=(20, 0), rowspan=4)
 
         # Generate image and save.
 
@@ -51,3 +61,58 @@ class TopRightFrame:
         print(f"Debug Log: Updated Image format to '{self.format_select_box.get()}'.")
 
         self.currently_selected_format = self.format_select_box.get()
+
+    def generate_image(self) -> None:
+        """
+        Generates an Image based on the object we currently have.
+        """
+
+        print("\nDebug Log: Generating image based on json file:")
+
+        image_width = self.main_menu_reference.middle_right_frame.image_width
+        image_height = self.main_menu_reference.middle_right_frame.image_height
+
+        # If the objects are empty, we set a placeholder image.
+        if not self.main_menu_reference.hierarchy:
+            self.main_menu_reference.middle_right_frame.update_image()
+            return
+
+        # We create the image from our c++ library/project.
+        if sys.platform.lower() == "linux" or sys.platform.lower() == "linux2":
+            # Linux Command
+            print("Debug Log: Platform has been identified as linux.")
+            _subp = subprocess.Popen("ping www.google.com", shell=True, stdout=subprocess.PIPE).communicate()[0]
+
+            # Now you would technically get the image location and load it from there.
+
+            # Simulating an image.
+            background_image = Image.open("data/generated/generated_2.jpg").resize((image_width, image_height))
+
+            image_ctk_b = customtkinter.CTkImage(background_image, size=(image_width, image_height))
+
+            self.main_menu_reference.middle_right_frame.update_image(image=image_ctk_b)
+
+        elif sys.platform.lower() == "windows" or sys.platform.lower() == "win32":
+            print("Debug Log: Platform has been identified as windows.")
+            _subp = subprocess.Popen("ping www.google.com", shell=True, stdout=subprocess.PIPE).communicate()[0]
+
+            # Now you would technically get the image location and load it from there.
+
+            # Simulating an image.
+            background_image = Image.open("data/generated/generated_2.jpg").resize((image_width, image_height))
+
+            image_ctk_b = customtkinter.CTkImage(background_image, size=(image_width, image_height))
+
+            self.main_menu_reference.middle_right_frame.update_image(image=image_ctk_b)
+
+        else:
+            print(f"Error Log: Unknown Platform {sys.platform}. Could not generate image, using fallback.")
+
+            background_image = Image.open("data/generated/generated.jpg").resize((image_width, image_height))
+
+            image_ctk_b = customtkinter.CTkImage(background_image, size=(image_width, image_height))
+
+            self.main_menu_reference.middle_right_frame.update_image(image=image_ctk_b)
+
+
+
