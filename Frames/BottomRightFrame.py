dc481ac7ffd025442290bd444babb1183900c350
Gasterbuzzer <juliankoberg@hotmail.com>
2023-08-16 14:09:52 +0200

    Added Saving Button

--- Frames/BottomRightFrame.py
+++ Frames/BottomRightFrame.py
@@ -3,6 +3,7 @@ import customtkinter
 from tkinter import filedialog
 from PIL import Image
 import os
+import json
 
 
 class BottomRightFrame:
@@ -40,17 +41,23 @@ class BottomRightFrame:
 
         self.label_location.grid_propagate(False)
 
-        # Image associated with the menu:
+        # Image associated with the Load Button:
         background_image = Image.open("data/icons/edit.png").resize((25, 25))
         image_ctk_b = customtkinter.CTkImage(background_image, size=(25, 25))
 
         self.image = customtkinter.CTkLabel(self.inside_frame, image=image_ctk_b, text="")
         self.image.grid(row=0, column=0, padx=(15, 0), pady=5)
 
-        # Button
+        # Load Button
         self.load_json_button = customtkinter.CTkButton(self.frame, text=f"Browse",
                                                         command=self.get_location_prompt, height=38)
         self.load_json_button.grid(row=0, column=1, padx=2, pady=5, sticky="nw")
+
+        # Save Button
+        # Saves the json changes from the user.
+        self.save_json_button = customtkinter.CTkButton(self.frame, text=f"Save changes",
+                                                        command=self.save_location_prompt, height=38)
+        self.save_json_button.grid(row=0, column=2, padx=(5, 2), pady=5, sticky="nw")
 
     def get_location_prompt(self) -> None:
         """
@@ -88,3 +95,32 @@ class BottomRightFrame:
 
         self.app.left_frame.create_hierarchy()
         self.app.left_frame.render_frame_objects()
+
+    def save_location_prompt(self) -> None:
+        """
+        Prompts the user for a location to store the
+        :return: (Str) Absolute Path to file.
+        """
+
+        # Request Input
+        json_file = filedialog.asksaveasfile(initialdir=os.getcwd(), title="Select location to store changes to "
+                                                                           "JSON File",
+                                             filetypes=(("JSON Files", "*.json"), ("all files", "*.*")),
+                                             defaultextension=".json")
+
+        # If the user did not select anything, we just state that no file was selected.
+        # Could be improved to
+        # return the currently selected one.
+        print(f"File Location: '{json_file}'.")
+        if json_file == "":
+            print("Weak Error: Save Location is invalid or does not exist.")
+            return
+
+        # Getting write data
+        json_values = {"test_value": 2}
+
+        # Writing
+        json.dump(json_values, json_file)
+
+        # Finished Saving
+        json_file.close()
