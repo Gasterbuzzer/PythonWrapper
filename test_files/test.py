6d02aeda6659bd09aad6ac069a6a6c792729b20d
Gasterbuzzer <juliankoberg@hotmail.com>
2023-09-10 10:15:18 +0200

    Update test.py

--- test_files/test.py
+++ test_files/test.py
@@ -1,35 +1,13 @@
-from tkinter import StringVar, TOP
-from tkinterdnd2 import TkinterDnD, DND_ALL
-import customtkinter as ctk
 
 
-class Tk(ctk.CTk, TkinterDnD.DnDWrapper):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.TkdndVersion = TkinterDnD._require(self)
+class test:
 
-def get_path(event) -> None:
-    """
+    def __init__(self):
 
-    :param event:
-    """
-    pathLabel.configure(text=event.data)
+        print(self)
+        print(hex(id(self)))
 
 
-# root = TkinterDnD.Tk()
-root = Tk()
-root.geometry("350x100")
-root.title("Get file path")
+test_ = test()
+exit(0)
 
-nameVar = StringVar()
-
-entryWidget = ctk.CTkEntry(root)
-entryWidget.pack(side=TOP, padx=5, pady=5)
-
-pathLabel = ctk.CTkLabel(root, text="Drag and drop file in the entry box")
-pathLabel.pack(side=TOP)
-
-entryWidget.drop_target_register(DND_ALL)
-entryWidget.dnd_bind("<<Drop>>", get_path)
-
-root.mainloop()
